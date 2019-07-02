1fb6a0104da66788feae30d15cded20352de045f
Jess K <48440717+grassgoplate@users.noreply.github.com>
2019-07-02 15:11:51 +0800

    Update msgshow.php
    1. <style> 改成用 <link href="style.css" rel="stylesheet">呼叫.
    2. 新增奇偶數列不同bgcolor.

--- msgshow.php
+++ msgshow.php
@@ -1,29 +1,39 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Message Board</title></head>
+<title>Message Board</title>
+<link href="style.css" rel="stylesheet">
+</head>
 <body>
-<h1 align=center>留言板</h1>
 <center>
+<div class=box2>
+<h1 align=center>留言板</h1>
+</div>
+<div class=box1>
+<br>
 <a href="msgpost.htm" align=center>我要留言</a>
 <table border="1" align="center" width=800>
-  <tr>
-    <th>編號</th>
-    <th>留言者</th>
+  <tr class="bg1">
+    <th width=40>編號</th>
+    <th width=120>留言者</th>
     <th>信箱</th>
     <th>留言日期時間</th>
     <th>留言內容</th>
   </tr>
 <?php 
- require("connmysql.php");
-  mysql_query("SET NAMES 'utf8'");//設定字元集與連線校對
-  $seldb = @mysql_select_db("msgboard");
-  if (!$seldb) die("資料庫選擇失敗！");
+  //require("connmysql.php");//連結與開啟資料庫
+	include "connmysql.php";
   $sql_query = "SELECT * FROM `qqq`";
   $result = mysql_query($sql_query);
   $total_records = mysql_num_rows($result);
+  $n=1;
   while($row_result=mysql_fetch_assoc($result)){
-    echo "<tr>";
+    if($n % 2==1){
+	  echo "<tr bgcolor=#C9FFFF>";
+	}else{
+      echo "<tr bgcolor=#F7FFCE>";
+    }
+	$n++;
     echo "<td>".$row_result["編號"]."</td>";
     echo "<td>".$row_result["名稱"]."</td>";
     echo "<td>".$row_result["EMAIL"]."</td>"; //大小寫須與DB同
@@ -33,6 +43,9 @@
   }
 ?>
 </table>
-<a href="msgpost.htm" align=center>我要留言</a>
+<br>
+</div>
+<!--
+<a href="msgpost.htm" align=center>我要留言</a>-->
 </body>
 </html>
