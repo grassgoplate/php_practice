46e03760149e94b41202a80453c366d84fadf2ce
Jess K <48440717+grassgoplate@users.noreply.github.com>
2019-07-02 15:04:05 +0800

    Update connmysql.php
    如果開啟失敗,則顯示錯誤並結束

--- connmysql.php
+++ connmysql.php
@@ -5,4 +5,7 @@ header("Content-Type: text/html; charset=utf-8");
   $db_password = "usbw";
   $db_link = @mysql_connect($db_host, $db_username, $db_password);
   if (!$db_link) die("資料庫連結失敗！"); //如果連結失敗,則顯示錯誤並結束
+ mysql_query("SET NAMES 'utf8'"); //設定字元集與連線校對
+  $seldb = @mysql_select_db("msgboard");
+  if (!$seldb) die("開啟資料庫失敗！"); //如果開啟失敗,則顯示錯誤並結束
   ?>
